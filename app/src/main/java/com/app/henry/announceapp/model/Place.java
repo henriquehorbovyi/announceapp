e87c0bab49507659fcf63649a37254028315f7a7
henrywm <carloshenry.wm@gmail.com>
2017-04-03 00:14:49 -0300

    Place Model done

--- app/src/main/java/com/app/henry/announceapp/model/Place.java
+++ app/src/main/java/com/app/henry/announceapp/model/Place.java
@@ -5,4 +5,46 @@ package com.app.henry.announceapp.model;
  */
 
 public class Place {
+    private int     id;
+    private String  name;
+    private String  location;
+    private int     img;
+
+    public Place(String name, String location, int img) {
+        this.name       = name;
+        this.location   = location;
+        this.img        = img;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
 }
