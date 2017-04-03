f4c92577dd2408b7d17ad2f17de6d0648bffe80a
henrywm <carloshenry.wm@gmail.com>
2017-04-03 00:15:37 -0300

    PlacesActivity done! :)

--- app/src/main/java/com/app/henry/announceapp/PlacesActivity.java
+++ app/src/main/java/com/app/henry/announceapp/PlacesActivity.java
@@ -3,11 +3,17 @@ package com.app.henry.announceapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import com.app.henry.announceapp.fragment.PlaceFragment;
+import com.app.henry.announceapp.model.Place;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlacesActivity extends AppCompatActivity {
 
@@ -19,6 +25,14 @@ public class PlacesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_places);
         toolbar = (Toolbar) findViewById(R.id.placestoolbar);
         setSupportActionBar(toolbar);
+
+        PlaceFragment fragment   =  (PlaceFragment) getSupportFragmentManager().findFragmentByTag("mainFrag");
+        if(fragment == null){
+            fragment = new PlaceFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_places,fragment,"mainFrag");
+            ft.commit();
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -37,4 +51,15 @@ public class PlacesActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    public List<Place> createPlaceList(int qtd){
+        List<Place> list = new ArrayList<>();
+        String name[]       = new String[]{"Super mercado Exemplo","Estabelecimento Tal","Loja Tal","Cinema Tal","Exemplo de Empresa"};
+        int    img[]        = new int[]{R.drawable.city,R.drawable.city,R.drawable.city,R.drawable.city,R.drawable.city};
+        String location[]   = new String[]{"55.459432,32.342394","55.459432,32.342395","55.49432,32.34394","55.459432,32.342394","55.459432,32.342394"};
+        for(int i = 0; i < qtd; i++){
+            Place p = new Place(name[i % name.length],location[i % location.length], img[i % img.length]);
+            list.add(p);
+        }
+        return list;
+    }
 }
