b9e38c5a3a0f8c0a7dfbabcca393759ddd75edea
henrywm <carloshenry.wm@gmail.com>
2017-04-22 17:49:01 -0300

    show details of a place here

---
+++ app/src/main/java/com/app/henry/announceapp/PlaceActivity.java
@@ -0,0 +1,13 @@
+package com.app.henry.announceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place);
+    }
+}
